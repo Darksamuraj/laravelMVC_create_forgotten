722f9794b64c1b0ccee95221dd0abe76cd502b98
Rin <30601759+Darksamuraj@users.noreply.github.com>
2018-05-16 20:23:02 +0500

    Routes_views
    Add some routes and views

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,15 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+/* Добавил маршруты */
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/one', function () {
+    return view('one');
+});
+Route::get('/two', function () {
+    return view('two');
+});
+Route::get('/three', function () {
+    return view('three');
 });
